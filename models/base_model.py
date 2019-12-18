7df2ac80c52bcf563e465bd065d68137314aefe8
valerc123 <valentina.rua123@gmail.com>
2019-12-18 00:14:55 +0000

    task 6

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,20 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+    id = Column('id', Integer, primary_key=True, nullable=False, unique=True)
+    created_at = Column('created_at', DateTime, default=datetime.utcnow)
+    updated_at = Column('updated_at', DateTime, default=datetime.utcnow)
+
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -29,12 +37,11 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
-        """returns a string
+        """Returns a string
         Return:
-            returns a string of class name, id, and dictionary
+            Returns a string of class name, id, and dictionary
         """
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
@@ -45,13 +52,14 @@ class BaseModel:
         return self.__str__()
 
     def save(self):
-        """updates the public instance attribute updated_at to current
+        """Updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """creates dictionary of the class  and returns
+        """Creates dictionary of the class  and returns
         Return:
             returns a dictionary of all the key values in __dict__
         """
@@ -59,4 +67,12 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+
+        if '_sa_instance_state' in my_dict:
+            del my_dict['_sa_instance_state']
         return my_dict
+
+    def delete(self):
+        # can be object
+        models.storage.delete(self.to_dict())
+
--- models/state.py
+++ models/state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
