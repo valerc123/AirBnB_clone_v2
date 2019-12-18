9222ab7915ffe4f620ecdf5b576dae63549604a1
valerc123 <valentina.rua123@gmail.com>
2019-12-18 22:42:08 +0000

    Create relationship

--- models/state.py
+++ models/state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This is the state class"""
+"""State Class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+from models.state import State
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -11,4 +13,13 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            city_all = models.storage.all('City').all()
+            n_city = []
+            for value in city_all.values():
+                if self.id == values.state_id:
+                    n_city.append(value)
+    else:
+        cities = relationship('City', cascade="delete", backref="State")
