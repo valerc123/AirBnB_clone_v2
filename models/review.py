49c78a367f32aaebbd939516462fa4d4a81e5964
guxal <jonathanacp93@gmail.com>
2019-12-20 21:58:09 +0000

    add condition models/review

--- models/review.py
+++ models/review.py
@@ -12,6 +12,11 @@ class Review(BaseModel, Base):
         text: review description
     """
     __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if storage_type == 'db':
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
