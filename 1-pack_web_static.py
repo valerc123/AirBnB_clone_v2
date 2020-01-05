04f0f8add3fd4875e6dee2858430844e248fe9e1
valerc123 <valentina.rua123@gmail.com>
2020-01-05 00:49:40 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-from fabric.io import local
+from datetime import datetime
+from fabric.api import local
 
 def do_pack():
-    local("ls -l")
-
+    time_now = datetime.now()
+    files = "web_static_{}{}{}{}{}{}.tgz".format(time_now.year,
+						 time_now.month,
+						 time_now.day,
+						 time_now.hour,
+						 time_now.minute,
+						 time_now.second)
+    try:
+        local("mkdir -p versions")
+        local("tar -cavf versions/{} web_static".format(files))
+        return "versions/{}".format(files)
+    except Exception:
+        return False
+    
