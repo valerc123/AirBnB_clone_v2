0ba2fad9db1c85c2007cc701f70c722107ad8138
valerc123 <valentina.rua123@gmail.com>
2020-01-14 22:59:27 +0000

    pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,11 +12,12 @@ def do_pack():
     Compress all files before sending
     """
     time_now = datetime.now()
-    files = "web_static_{}{}{}{}{}{}.tgz".format(time_now.year, time_now.month,
-            time_now.day, time_now.hour, time_now.minute, time_now.second)
+    files = "web_static_{}{}{}{}{}{}.tgz".format(
+            time_now.year, time_now.month, time_now.day,
+            time_now.hour, time_now.minute, time_now.second)
     try:
         local("mkdir -p versions")
         local("tar -cavf versions/{} web_static".format(files))
         return "versions/{}".format(files)
     except Exception:
-        return False
+        return None
