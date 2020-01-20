2b18eb0f7067c287d1aaf0b8d12ccee6f2495066
valerc123 <valentina.rua123@gmail.com>
2020-01-20 18:42:33 +0000

    C Route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def app_1():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def app_2(text):
+    return "C {}".format(text.replace('_', ' '))
+
+app.run()
