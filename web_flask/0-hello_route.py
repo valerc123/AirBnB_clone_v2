c67b3d6ebdb9691dd6f38d603106b0abca70901d
valerc123 <valentina.rua123@gmail.com>
2020-01-20 17:11:10 +0000

    start a flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return 'Hello HBNB!'
+
+app.run()
