f8912b065feb3ffe4fe7092fa01ba3023847ab08
valerc123 <valentina.rua123@gmail.com>
2020-01-22 19:09:53 +0000

    cities by state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application
+"""
+from flask import Flask, render_template
+import models
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_sess(self):
+    models.storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def storage():
+    store = models.storage.all("State")
+    return render_template('7-states_list.html', states=store)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    obj = models.storage.all('City')
+    return render_template('8-cities_by_states.html', states=obj)
+
+if __name__ == "__main__":
+    app.run(debug=True)
