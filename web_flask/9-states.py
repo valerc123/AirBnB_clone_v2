d5157b47d386721b88ff351befacf17a4cbfe58b
valerc123 <valentina.rua123@gmail.com>
2020-01-22 22:59:58 +0000

    tryibg

---
+++ web_flask/9-states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application
+"""
+from flask import Flask, render_template
+import models
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_sess(self):
+    models.storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def storage():
+    store = models.storage.all("State")
+    return render_template('7-states_list.html', states=store)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    obj = models.storage.all('State')
+    return render_template('8-cities_by_states.html', states=obj)
+
+
+@app.route('/states/<int:id>', strict_slashes=False)
+def states(id):
+    obj = models.store.all('State')
+    return render_template('9-states.html', states=obj, id=id)
+
+
+#@app.route('/states/<int:id>', strict_slashes=False)
+#def state_id():
+
+ #   return rendere_template('9-states.html', states=obj)
+if __name__ == "__main__":
+    app.run(debug=True)
